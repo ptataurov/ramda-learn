6b9b1b55899f22ce913a3b699c20804a8263cd37
Pavel <p.tataurov92@gmail.com>
2020-03-26 15:51:02 +0300

    commit

---
+++ index.js
@@ -0,0 +1,55 @@
+const R = require("ramda");
+
+const arr = [1, 2, 3];
+
+const users = [
+  {
+    id: 1,
+    name: "Joe"
+  },
+  {
+    id: 2,
+    name: "Alex"
+  },
+  {
+    id: 3,
+    name: "Bill"
+  }
+];
+
+// append
+// find
+// propEq
+// both
+// either
+// split
+// toLower
+// toUpper
+// join
+// compose
+// path
+// pathOr
+// converge
+// equals
+// head
+// descend
+// identity
+// where
+// pluck
+// __
+// lt
+// gt
+// ifElse
+// concat
+// always
+// when
+// take
+// propSatisfies
+// unless
+// lens
+// lensProp
+// prop
+// assoc
+// view
+// set
+// over
